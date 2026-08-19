d4c57c43daffd937dc651b0b5729c7cebf66d1e8
Santiago Pascual <santipascual2009@gmail.com>
2026-08-19 13:49:46 -0300

    modified test session engine

--- tests/test_session_engine.py
+++ tests/test_session_engine.py
@@ -8,6 +8,7 @@ def test_session_boundaries():
     timestamps = pd.to_datetime([
         "2025-01-06 09:29:00",
         "2025-01-06 09:30:00",
+        "2025-01-06 09:31:00",
         "2025-01-06 16:59:00",
         "2025-01-06 17:00:00",
         "2025-01-06 17:59:00",
@@ -48,4 +49,47 @@ def test_session_boundaries():
         .astype(str)
         .tolist()
         == expected_session_dates
-    )
+        )
+
+    assert df.loc[
+        df["market_period"] == "RTH",
+        "minutes_since_rth_open"
+    ].tolist() == [
+        0.0,
+        1.0,
+        449.0
+        ]   
+
+    assert df.loc[
+        df["market_period"] == "RTH",
+        "minutes_until_rth_close"
+    ].tolist() == [
+        450.0,
+        449.0,
+        1.0
+        ]
+
+    assert df.loc[
+        df["market_period"] == "RTH",
+        "rth_progress"
+    ].tolist() == [
+        0.0,
+        1 / 450,
+        449 / 450,
+    ]
+
+    assert df.loc[
+        df["market_period"] != "RTH",
+        "minutes_since_rth_open"
+    ].isna().all()
+
+    assert df.loc[
+            df["market_period"] != "RTH",
+            "minutes_since_rth_close"
+        ].isna().all()
+
+    assert df.loc[
+            df["market_period"] != "RTH",
+            "rth_progress"
+        ].isna().all()
+
